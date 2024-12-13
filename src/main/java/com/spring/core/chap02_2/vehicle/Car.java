1efa6e8cdc76c4663f0a8dc4c2cb17beb57c7817
soongu <naong526@naver.com>
2024-12-13 10:50:26 +0900

    #2-2. 의존성 주입

---
+++ src/main/java/com/spring/core/chap02_2/vehicle/Car.java
@@ -0,0 +1,24 @@
+package com.spring.core.chap02_2.vehicle;
+
+public class Car {
+
+    //의존 객체 설정
+    private Engine engine;
+
+    // 생성자 주입 - 생성자를 통해 의존객체를 전달받음
+//    public Car(Engine engine) {
+//        this.engine = engine;
+//    }
+
+    public Car() {
+    }
+   // setter 주입 - setter를 통해 의존객체를 전달받음
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void startEngine() {
+        engine.start();
+        System.out.println("자동차가 달릴 준비가 완료되었습니다.");
+    }
+}
